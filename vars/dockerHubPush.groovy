fe3e0e371394fc7cb6e25188e8e1451562fda42c
Nick Stenning <nick@whiteink.com>
2017-06-13 13:42:39 +0200

    Can't define locally scoped variables in vars/
    The contents of vars/ Groovy files are instantiated as singletons, so
    adding stuff to the top scope doesn't have the effect you might expect
    if you were running this as a script.
    
    Instead, use named keywords to clarify the purpose of these parameters.

--- vars/dockerHubPush.groovy
+++ vars/dockerHubPush.groovy
@@ -1,12 +1,10 @@
 #!groovy
 
-// The ID of the stored credentials for the Docker Hub
-def dockerHubCredentialsID = 'docker-hub-build'
-
 // Push a pre-tagged docker build to the Docker Hub, and also update the
 // `latest` tag.
 def call(image) {
-    docker.withRegistry('', dockerHubCredentialsID) {
+    // Blank URL uses the public Docker Hub.
+    docker.withRegistry(url: '', credentialsId: 'docker-hub-build') {
         image.push()
         image.push('latest')
     }
